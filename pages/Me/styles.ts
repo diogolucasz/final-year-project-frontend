7c4b5cbf80696ebf60840da6981e9b9bfd0335fb
Diogo Lucas <diogolucas@outlook.pt>
2022-02-17 17:17:50 +0000

    Cleaning some files

---
+++ pages/Me/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100%;
+
+    @media (min-width: 1024) {
+        > main {
+            margin: 0 2rem;
+            display: flex;
+            justify-content: center;
+        }
+
+        
+    }
+`
+
+

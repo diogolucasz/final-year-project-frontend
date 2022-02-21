e3f0514e159ac24a0ebe99ff172ce33d3bcf58ca
Diogo Lucas <diogolucas@outlook.pt>
2022-02-21 22:45:33 +0000

    Authentication completed

---
+++ context/AuthContext.tsx
@@ -0,0 +1,105 @@
+import Router from "next/router";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+interface User {
+    email: string;
+    permissions: string[];
+    roles: string[];
+}
+
+interface SignInData {
+    email: string;
+    password: string;
+}
+
+interface AuthContextData {
+    SignIn(data: SignInData): Promise<void>;
+    user: User;
+    isAuthenticated: boolean;
+}
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function signOut() {
+
+    destroyCookie(undefined, 'fyp.token')
+    destroyCookie(undefined, 'fyp.refreshToken')
+
+    Router.push('/signin')
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+
+    const [user, setUser] = useState<User>();
+    const isAuthenticated = !!user;
+
+    useEffect(() => {
+
+        const { 'fyp.token': token } = parseCookies();
+
+        if (token) {
+
+            api.get('/me').then(response => {
+
+                const { email, permissions, roles } = response.data;
+
+                setUser({ email, permissions, roles })
+
+            }).catch(() => {
+
+                signOut();
+
+            })
+        }
+
+    }, [])
+
+
+    async function SignIn({ email, password }: SignInData) {
+
+        try {
+
+            const response = await api.post('sessions', {
+                email,
+                password,
+            })
+
+            const { token, refreshToken, permissions, roles } = response.data;
+
+            setUser({
+                email,
+                permissions,
+                roles,
+            })
+
+            setCookie(undefined, 'fyp.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/',
+            });
+
+            setCookie(undefined, 'fyp.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/',
+            });
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+            Router.push('/dashboard')
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ SignIn, isAuthenticated, user }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
---
+++ pages/signin.tsx
@@ -0,0 +1,53 @@
+import { GetServerSideProps } from "next";
+import { redirect } from "next/dist/server/api-utils";
+import { parseCookies } from "nookies";
+import { FormEvent, useContext, useState } from "react"
+import { AuthContext } from "../context/AuthContext";
+
+export default function SignIn() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const { SignIn } = useContext(AuthContext);
+
+    async function handleSubmit(event: FormEvent) {
+
+        event.preventDefault()
+
+        const data = {
+            email,
+            password
+        }
+
+        await SignIn(data);
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <button type="submit">Entrar</button>
+        </form>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+    const cookies = parseCookies(ctx);
+
+    if (cookies['fyp.token']) {
+
+        return {
+            redirect: {
+                destination: '/dashboard',
+                permanent: false,
+            }
+        }
+    }
+
+    return{
+        props: {}
+    }
+}
